992f639eb4d549a2dfd0f05e3041d6ac0f05498e
Daniel Moldovan <d.moldovan@dsg.tuwien.ac.at>
2014-09-15 10:32:32 +0200

    Maintaining also message queue config api

---
+++ MELA-Core/MELA-DataService/src/main/java/at/ac/tuwien/dsg/celar/mela/dataservice/api/MessageQueueAPI.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package at.ac.tuwien.dsg.celar.mela.dataservice.api;
+import at.ac.tuwien.dsg.mela.dataservice.api.CommandConsumer;
+import org.springframework.stereotype.Component;
+/**
+ *
+ * @author Daniel Moldovan E-Mail: d.moldovan@dsg.tuwien.ac.at
+ */
+@Component
+public class MessageQueueAPI extends CommandConsumer{
+
+}
