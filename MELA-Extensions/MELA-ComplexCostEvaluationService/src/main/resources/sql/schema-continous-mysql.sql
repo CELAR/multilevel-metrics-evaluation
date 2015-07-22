9216ba3ff1ee646805bb6f87ac16b5d0abae703e
Daniel Moldovan <d.moldovan@dsg.tuwien.ac.at>
2015-07-22 12:33:53 +0200

    fixed yet another issue with sql script

--- MELA-Extensions/MELA-ComplexCostEvaluationService/src/main/resources/sql/schema-continous-mysql.sql
+++ MELA-Extensions/MELA-ComplexCostEvaluationService/src/main/resources/sql/schema-continous-mysql.sql
@@ -2,8 +2,8 @@ create table IF NOT EXISTS CaschedHistoricalUsage (ID int AUTO_INCREMENT PRIMARY
 create table IF NOT EXISTS CaschedCompleteHistoricalUsage (ID int AUTO_INCREMENT PRIMARY KEY, monSeqID VARCHAR(200), timestampID int, data  LONGBLOB, FOREIGN KEY (monSeqID) REFERENCES MonitoringSeq(ID), FOREIGN KEY (timestampID) REFERENCES Timestamp(ID) );
 create table IF NOT EXISTS InstantCostHistory (ID int AUTO_INCREMENT PRIMARY KEY, monSeqID VARCHAR(200), timestampID int, data  LONGBLOB, FOREIGN KEY (monSeqID) REFERENCES MonitoringSeq(ID), FOREIGN KEY (timestampID) REFERENCES Timestamp(ID) );
 create table IF NOT EXISTS TotalCostHistory (ID int AUTO_INCREMENT PRIMARY KEY, monSeqID VARCHAR(200), timestampID int, data  LONGBLOB, FOREIGN KEY (monSeqID) REFERENCES MonitoringSeq(ID), FOREIGN KEY (timestampID) REFERENCES Timestamp(ID) );
-create table IF NOT EXISTS InstantCostElasticitySpace (ID int AUTO_INCREMENT PRIMARY KEY, monSeqID VARCHAR(200),  startTimestampID int, endTimestampID int, elasticitySpace LONGBLOB, FOREIGN KEY (monSeqID) REFERENCES MonitoringSeq(ID), FOREIGN KEY (startTimestampID) REFERENCES Timestamp(ID), FOREIGN KEY (endTimestampID) REFERENCES Timestamp(ID) )
-create table IF NOT EXISTS InstantCostElasticityPathway (ID int AUTO_INCREMENT PRIMARY KEY, monSeqID VARCHAR(200),  timestampID int, elasticityPathway LONGBLOB, FOREIGN KEY (monSeqID) REFERENCES MonitoringSeq(ID),FOREIGN KEY (timestampID) REFERENCES Timestamp(ID) )
+create table IF NOT EXISTS InstantCostElasticitySpace (ID int AUTO_INCREMENT PRIMARY KEY, monSeqID VARCHAR(200),  startTimestampID int, endTimestampID int, elasticitySpace LONGBLOB, FOREIGN KEY (monSeqID) REFERENCES MonitoringSeq(ID), FOREIGN KEY (startTimestampID) REFERENCES Timestamp(ID), FOREIGN KEY (endTimestampID) REFERENCES Timestamp(ID) );
+create table IF NOT EXISTS InstantCostElasticityPathway (ID int AUTO_INCREMENT PRIMARY KEY, monSeqID VARCHAR(200),  timestampID int, elasticityPathway LONGBLOB, FOREIGN KEY (monSeqID) REFERENCES MonitoringSeq(ID),FOREIGN KEY (timestampID) REFERENCES Timestamp(ID) );
 
 
 
