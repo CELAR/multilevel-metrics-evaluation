e02b217716cfe122406036f26446b6838f8dbbbc
Daniel Moldovan <d.moldovan@dsg.tuwien.ac.at>
2015-11-04 16:07:36 +0100

    fixed cost installer to point to correct directory

--- MELA-Extensions/MELA-ComplexCostEvaluationService-tar/scripts/installer.sh
+++ MELA-Extensions/MELA-ComplexCostEvaluationService-tar/scripts/installer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-MELA_ANALYSIS_DIR=/opt/mela-analysis-service
+MELA_ANALYSIS_DIR=/opt/mela-cost-eval-service
 
 mkdir -p $MELA_ANALYSIS_DIR
 cp ./*jar $MELA_ANALYSIS_DIR
 cp -R ./config $MELA_ANALYSIS_DIR
 
-cp ./mela-analysis-service /etc/init.d/
+cp ./mela-cost-eval-service/etc/init.d/
 chmod +x /etc/init.d/mela-analysis-service
-update-rc.d -f mela-analysis-service defaults
+update-rc.d -f mela-cost-eval-service defaults
